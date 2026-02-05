0b75e6604f89486be9d28ede3914d76e076907ba
nkunamphogelo,-lab <nkunamphogelo@gmail.com>
2026-02-05 15:18:08 +0200

    Added data folder inside added a class applicationdb

---
+++ VenueBookingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VenueBookingApp.Models;
+
+namespace VenueBookingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Venue> Venues { get; set; }
+    }
+}
