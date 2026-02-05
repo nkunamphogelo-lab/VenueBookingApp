f176dc1e712456caf5d7728cb50c82a87f254213
nkunamphogelo,-lab <nkunamphogelo@gmail.com>
2026-02-05 14:34:34 +0200

    Add project files.

---
+++ VenueBookingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VenueBookingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
